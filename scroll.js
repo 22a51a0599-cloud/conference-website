5e37c8016dfad19f16b77aea2ec5884f5146f4b8
22a51a0599-cloud <22a51a0599@adityatekkali.edu.in>
2026-01-18 14:46:48 +0530

    Add files via upload

---
+++ scroll.js
@@ -0,0 +1,14 @@
+const sections = document.querySelectorAll('.section');
+
+function reveal(){
+  sections.forEach(section=>{
+    const top = section.getBoundingClientRect().top;
+    if(top < window.innerHeight - 100){
+      section.style.opacity = "1";
+      section.style.transform = "translateY(0)";
+    }
+  });
+}
+
+window.addEventListener("scroll", reveal);
+reveal();
